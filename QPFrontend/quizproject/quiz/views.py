3f90bcbe3c01ce6dbea4c501e66ce2855623a435
FuneralClown <funeralclown@yandex.ru>
2024-04-09 12:29:55 +0200

    refactoring, removing a direct api call by url

--- QPFrontend/quizproject/quiz/views.py
+++ QPFrontend/quizproject/quiz/views.py
@@ -1,27 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-import requests
-from django.views.decorators.csrf import requires_csrf_token, csrf_exempt
-from django.views.generic import ListView
+from django.views.decorators.csrf import csrf_exempt
 from .response_answers import ResponseAnswers
+from .qpbackend.qpbackend import QPBackend
 
 
 def index(request):
     """
         Обработчик для главной страницы
     """
+    quiz_data = QPBackend().get_quiz()
 
-    response = requests.get('http://127.0.0.1:4000/api/v1/quiz/')
-    quiz = response.json()
     return render(request, 'quiz/index.html'
-                  , {'title': 'Index', 'quiz': quiz})
+                  , {'title': 'Index', 'quiz': quiz_data})
 
 
 def about(request):
     """
         Обработчик для страницы about
     """
-
     return render(request, 'quiz/about.html',
                   {'title': 'about'})
 
@@ -30,9 +26,8 @@ def quiz(request):
     """
         Обработчик для списка тестов
     """
+    quiz_data = QPBackend().get_quiz()
 
-    response = requests.get('http://127.0.0.1:4000/api/v1/quiz/')
-    quiz_data = response.json()
     return render(request, 'quiz/quiz.html'
                   , {'title': 'Quiz', 'quiz': quiz_data[::-1]})
 
@@ -41,8 +36,7 @@ def quiz_change(request):
     """
         Обработчик для страницы редактора тестов
     """
-    response = requests.get('http://127.0.0.1:4000/api/v1/quiz/')
-    quiz_data = response.json()
+    quiz_data = QPBackend().get_quiz()
     return render(request, 'quiz/quiz_change.html'
                   , {'title': 'Quiz', 'quiz': quiz_data[::-1]})
 
@@ -54,7 +48,8 @@ def quiz_create(request):
     """
     if request.POST:
         json_data = {'title': request.POST['title'], 'description': request.POST['description']}
-        requests.post('http://127.0.0.1:4000/api/v1/quiz/', json=json_data)
+        QPBackend().post_quiz(json_data)
+
         return redirect('quiz_change/')
 
     return render(request, 'quiz/quiz_create.html'
@@ -68,12 +63,10 @@ def quiz_update(request, quiz_id):
     """
     if request.POST:
         json_data = {'title': request.POST['title'], 'description': request.POST['description']}
-        print(json_data, quiz_id)
-        requests.put(f'http://127.0.0.1:4000/api/v1/quiz/{quiz_id}/', json=json_data)
+        QPBackend(quiz_id).put_quiz(json_data)
         return redirect(quiz_change)
 
-    response = requests.get(f'http://127.0.0.1:4000/api/v1/quiz/{quiz_id}/')
-    quiz_data = response.json()
+    quiz_data = QPBackend(quiz_id).get_quiz()
     return render(request, 'quiz/quiz_update.html'
                   , {'title': 'QuizUpdate', 'quiz': quiz_data})
 
@@ -82,7 +75,7 @@ def quiz_delete(request, quiz_id):
     """
             Обработчик удаления теста
     """
-    requests.delete(f'http://127.0.0.1:4000/api/v1/quiz/{quiz_id}/')
+    QPBackend(quiz_id).delete_quiz()
     return redirect(quiz_change)
 
 
@@ -90,8 +83,7 @@ def questions(request, quiz_id=1):
     """
         Обработчик для страницы со списком вопросов теста pk = quiz_id
     """
-    response = requests.get(f'http://127.0.0.1:4000/api/v1/quizquestions/{quiz_id}/')
-    questions_data = response.json()
+    questions_data = QPBackend(quiz_id).get_quiz_questions()
     return render(request, 'quiz/questions.html'
                   , {'title': 'Questions', 'quiz_id': quiz_id, 'questions': questions_data[f'questions']})
 
@@ -101,8 +93,7 @@ def question_change(request, quiz_id=1):
     """
         Обработчик для страницы редактора вопросов
     """
-    response = requests.get(f'http://127.0.0.1:4000/api/v1/quizquestions/{quiz_id}/')
-    questions_data = response.json()
+    questions_data = QPBackend(quiz_id).get_quiz_questions()
     return render(request, 'quiz/questions_change.html'
                   , {'title': 'QuestionsChange', 'quiz_id': quiz_id, 'questions': questions_data[f'questions']})
 
@@ -114,7 +105,7 @@ def question_create(request, quiz_id=2):
     """
     if request.POST:
         json_data = {'text': request.POST['text'], 'correct_answer': request.POST['correct_answer'], 'quiz': quiz_id}
-        requests.post('http://127.0.0.1:4000/api/v1/question/', json=json_data)
+        QPBackend().post_question(json_data)
         return redirect(question_change, quiz_id=quiz_id)
 
     return render(request, 'quiz/question_create.html'
@@ -125,7 +116,7 @@ def question_delete(request, quiz_id, question_id):
     """
         Обработчик удаления вопроса
     """
-    requests.delete(f'http://127.0.0.1:4000/api/v1/question/{question_id}/')
+    QPBackend(quiz_id).delete_question()
     return redirect(question_change, quiz_id=quiz_id)
 
 
@@ -133,8 +124,7 @@ def result(request, quiz_id):
     """
         Обработчик для страницы результатов/аналитики теста
     """
-    response = requests.get(f'http://127.0.0.1:4000/api/v1/quizinfo/{quiz_id}/')
-    quizinfo_data = response.json()
+    quizinfo_data = QPBackend(quiz_id).get_quiz_info()
     return render(request, 'quiz/quizinf.html'
                   , {'title': 'QuizInfo', 'quiz_info': quizinfo_data['quiz_info']})
 
